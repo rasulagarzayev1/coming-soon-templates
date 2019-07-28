a47e69d2165146b36ce45764177ba937118530c1
rasulagarzayev1 <41019761+rasulagarzayev1@users.noreply.github.com>
2019-07-28 18:17:49 +0400

    Add files via upload

---
+++ New folder2/comingsooon.js
@@ -0,0 +1,12 @@
+
+(function($) {
+    "use strict";
+    if($('.time-countdown').length){  
+            $('.time-countdown').each(function() {
+            var $this = $(this), finalDate = $(this).data('countdown');
+            $this.countdown(finalDate, function(event) {
+                var $this = $(this).html(event.strftime('' + '<div class="counter-column"><span class="count">%D</span>Days</div> ' + '<div class="counter-column"><span class="count">%H</span>Hours</div> ' + '<div class="counter-column"><span class="count">%M</span>Minutes</div>  ' + '<div class="counter-column"><span class="count">%S</span>Seconds</div>'));
+            });
+         });
+        }
+})(window.jQuery);
